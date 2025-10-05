71ab1213bebb34beddc149131b3450ded63ed8b8
Saima Khan <saimakhancsedu@gmail.com>
2025-10-05 21:03:28 +0600

    edited

--- src/pages/BookDetails/BookDetails.jsx
+++ src/pages/BookDetails/BookDetails.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useLoaderData, useParams } from "react-router";
+import { addToStoredDB } from "../../utility/addToDB";
 
 const BookDetails = () => {
   const { id } = useParams();
@@ -10,13 +11,27 @@ const BookDetails = () => {
   const singleBook = data.find((book) => book.bookId === bookId);
   // console.log(singleBook);
   const { bookName, image } = singleBook;
+
+  const handleMarksAsRead = () => {
+    //store with id
+    //where to store?
+    //array or collection
+    //if book already exists, then show and alert
+    //if book does not exist, then push it in the collection or array
+    addToStoredDB(id);
+  };
   return (
     <div className=" w-2/3 h-[400px] ">
       <img className="w-48" src={image} alt="" />
       <h2>{bookName}</h2>
 
-      <button className="btn btn-accent m-2">Read</button>
-      <button className="btn btn-accent m-2">Wish List</button>
+      <button
+        onClick={() => handleMarksAsRead(id)}
+        className="btn btn-accent m-2"
+      >
+        Marks as Read
+      </button>
+      <button className="btn btn-accent m-2">Add to Wish List</button>
     </div>
   );
 };
